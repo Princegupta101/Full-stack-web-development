b84e7754c35d3bfb91c0d6ffad23f0041482d170
Princegupta101 <pgupta28349@gmail.com>
2023-08-20 16:06:32 +0530

    main

---
+++ workingofjs/13april.js
@@ -0,0 +1,38 @@
+// Question 1
+console.log(addNumbers(5,10));
+function addNumbers(num1, num2){
+    return num1+num2;
+}
+
+// Question 2
+console.log(multiplyNumber(3,4));
+const multiplyNumber = function(num1,num2){
+    return num1+num2;
+}
+
+// Question 3
+
+function addNumbers(num1,num2){
+    console.log(sum);
+    var sum;
+    sum=num1+num2;
+    return sum;
+}
+console.log(addNumbers(5,10));
+
+// Question 4
+
+console.log(x);
+console.log(y);
+console.log(z);
+var x="Pw";
+let y="skills";
+const z="!";
+
+// Question 5
+
+console.log(x);
+let x="hello";
+console.log(x);
+
+
